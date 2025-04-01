2871e7db3cd031d22d81a1a31ebfd2f4e7262b38
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-03-31 21:53:46 -0300

    Terceira Aula

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); // Biblioteca para criptografar senhas
 
 // Importando o modelo de Usuario
 const User = require("./models/usuarioModel");
@@ -11,22 +12,60 @@ const app = express();
 // Configura o express para entender req. em Json
 app.use(express.json());
 
+// Rota aberta
 app.get("/", (requisicao, resposta) => {
-  resposta.status(200).send(carro2025);
+  resposta.status(200).send({ msg: "Bem vindo a API!" });
 });
 
-app.get("/:sigla", (req, res) => {
-  const siglaInformada = req.params.sigla.toUpperCase(); // Obtém a sigla
-  const carro = carro2025.find(
-    (infoCarro) => infoCarro.sigla === siglaInformada
-  ); // Busca o carro pela sigla
-  if (!carro) {
-    // ! Vazio = NOT
-    // Se o carro não for encontrado, retorna 404.
-    res.status(404).send("Não existe um carro com essa sigla!");
-    return; // Para a função!
+// Post de cadastro de usuário
+app.post("/auth/register", async (req, res) => {
+  const { nome, email, password, confirmpassword } = req.body;
+
+  // Validações
+  if (!nome) {
+    return res.status(422).json({ msg: "O nome é obrigatório" });
   }
-  res.status(200).send(carro); // Retorna o carro encontrado
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "O password é obrigatório" });
+  }
+
+  if (password != confirmpassword) {
+    return res
+      .status(422)
+      .json({ msg: "A senha e a confirmação não conferem" });
+  }
+
+  // Busca se existe o usuario no DB
+  const userExists = await User.findOne({ email: email });
+
+  // Retorna uma mensagem para usuario de duplicidade
+  if (userExists) {
+    return res.status(422).json({ msg: "Por favor, utilize outro E-mail" });
+  }
+
+  // Criar a criptografia da senha
+  const salt = await bcrypt.genSalt(12); // Gera um salt para criptografar a senha
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Criar usuario conforme o Model
+  const user = new User({
+    nome,
+    email,
+    password: passwordHash, // Senha criptografada
+  });
+
+  try {
+    await user.save(); // Salva o usuario no banco de dados
+
+    res.status(201).json({ msg: "Usuario criado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ msg: "Erro" });
+  }
 });
 
 // Credenciais
