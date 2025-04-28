e3423505a68b743602ea01f28977bf51b3463163
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-04-28 20:41:25 -0300

    Aula 28-04-2025

---
+++ routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/authController");
+const validateRequest = require("../middlewares/validationMiddleware");
+const { registerSchema, loginSchema } = require("../validators/userValidator");
+
+router.post("/register", validateRequest(registerSchema), registerUser);
+
+router.login("/login", validateRequest(loginSchema), loginUser);
